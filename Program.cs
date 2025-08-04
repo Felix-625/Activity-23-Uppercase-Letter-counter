f14034610169a7d69f0c9a2e997b81027d82836c
Felix-625 <almerofelix62@gmail.com>
2025-08-04 13:36:31 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String sentence;
+
+            Console.WriteLine("Enter a sentence:");
+            sentence = Console.ReadLine();
+
+            int uppercase = 0;
+
+            // Loop through each character in the input string
+            foreach (char c in sentence)
+            {
+                // Check if the character is an uppercase letter
+                if (char.IsUpper(c))
+                {
+                    uppercase++;
+                }
+            }
+
+            // Display the result
+            Console.WriteLine("Total uppercase letters: " + uppercase);
+        }
+    }
+}
